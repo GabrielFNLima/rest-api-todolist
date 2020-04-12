65c37917a15658dc244f19baab08c126c46c811d
GabrielFNLima <gabrielnlima@outlook.com>
2020-04-12 14:00:59 -0300

    Config Routes

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const express = require('express')
 const PORT = process.env.PORT || 4000
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//app.get('/',(req,res)=>{
+//    res.json({message:"RESt API with Node.js, and Sqlite"})
+//})
+
+require('./routes/index')(app)
 
 app.listen(PORT, () => {
     console.log("hello")
 })
+
+
+module.exports = app
