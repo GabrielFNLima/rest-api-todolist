a36713406e4bdc73b589e29355b8657d777777c0
GabrielFNLima <gabrielfnlima@gmail.com>
2020-04-14 19:39:29 -0300

    Todo Delete

--- controller/todos.js
+++ controller/todos.js
@@ -4,37 +4,38 @@ const TodoItem = require("../models").TodoItem;
 module.exports = {
   create(req, res) {
     return Todo.create({
-      title: req.body.title
+      title: req.body.title,
     })
-      .then(todo => res.status(201).send(todo))
-      .catch(err => res.status(400).send(err));
+      .then((todo) => res.status(201).send(todo))
+      .catch((err) => res.status(400).send(err));
   },
   list(req, res) {
     return Todo.findAll({
       include: [
         {
           model: TodoItem,
-          as: "todoItems"
-        }
-      ]
+          as: "todoItems",
+        },
+      ],
     })
-      .then(todos => res.status(201).send(todos))
-      .catch(error => res.status(400).send(error));
+      .then((todos) => res.status(200).send(todos))
+      .catch((error) => res.status(400).send(error));
   },
   destroy(req, res) {
     return Todo.findByPk(req.params.todoId)
-      .then(todo => {
+      .then((todo) => {
         if (!todo) {
           return res.status(400).send({
-            message: "Todo List not found"
+            message: "Todo List not found",
           });
         }
-        return todo.destroy();
-        res
-          .status(200)
-          .send({ message: "Todo List deleted Successfully" })
-          .catch(error => res.status(400).send(error));
+        todo
+          .destroy()
+          .then(() =>
+            res.status(200).send({ message: "Todo List deleted Successfully" })
+          )
+          .catch((error) => res.status(400).send(error));
       })
-      .catch(error => res.status(400).send(error));
-  }
+      .catch((error) => res.status(400).send(error));
+  },
 };
